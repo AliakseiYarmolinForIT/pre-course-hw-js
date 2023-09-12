ceb8fe1ffae7d8dc3fd9f7c162ba88bb415cc870
AliakseiYarmolin <aliakseiyarmolinforit@gmail.com>
2023-09-12 18:41:55 +0300

    refresh 1 lesson 1 exercise 6

--- lesson-01/06.js
+++ lesson-01/06.js
@@ -1,7 +1,3 @@
 let str = "zzzzyzz";
 let arr = str.split("");
-if (arr.includes("y")) {
-    console.log("нашел");
-} else {
-    console.log("заблудился");
-}
+(arr.includes("y")) ? console.log("нашел") : console.log("заблудился");
