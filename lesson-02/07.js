433c50acb57b9e7700e25eef13a209aedf6d3b56
AliakseiYarmolin <aliakseiyarmolinforit@gmail.com>
2023-09-19 14:48:16 +0300

    lesson 2 exercise 7

---
+++ lesson-02/07.js
@@ -0,0 +1,15 @@
+let passportMarried = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+
+let newPassportMarried = structuredClone(passportMarried);
+
+newPassportMarried.married = true; 
+
+console.log(passportMarried);
+console.log(newPassportMarried);
