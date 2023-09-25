de4b03f8f2c2fca728a93e42d67ab037ca336cc5
AliakseiYarmolin <aliakseiyarmolinforit@gmail.com>
2023-09-25 16:47:52 +0300

    lesson 3 exersice 1

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,8 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    let flag = colors.filter((color) => color === "черный" || color === "красный" || color === "желтый");
+    let result = flag.join(-);
+    return result;
+}
+console.log(createColorString());
